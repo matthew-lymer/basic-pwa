dd903ab9f72889c390d216fe1ad6fbcec35f73cc
Matthew Lymer <matthewlymer@hotmail.co.uk>
2020-02-17 17:18:11 +0000

    Remove stupid console output

--- js/scripts.js
+++ js/scripts.js
@@ -2,9 +2,9 @@ const app = {
         init() {
             "serviceWorker" in navigator && window.addEventListener("load", function() {
                 navigator.serviceWorker.register("sw.js").then(function(e) {
-                    console.log("ServiceWorker registration successful with scope: ", e.scope)
+                    //
                 }, function(e) {
-                    console.log("ServiceWorker registration failed: ", e)
+                    //
                 })
             }), this.checkIOS()
         },
