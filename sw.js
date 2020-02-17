021e93a54beb4dc94b2be763bfa800dcab561714
Matthew Lymer <matthewlymer@hotmail.co.uk>
2020-02-17 15:48:41 +0000

    Initial setup

---
+++ sw.js
@@ -0,0 +1,38 @@
+const cacheName = 'PWA-sample-v1';
+const dynamicCacheName = 'site-dynamic-v1';
+const filesToCache = [
+  'index.html',
+  'css/style.css',
+  'js/scripts.js'
+];
+
+// cache size limit function
+const limitCacheSize = (name, size) => {
+  caches.open(name).then(cache => {
+    cache.keys().then(keys => {
+      if(keys.length > size){
+        cache.delete(keys[0]).then(limitCacheSize(name, size));
+      }
+    });
+  });
+};
+
+self.addEventListener('install', evt => {
+  evt.waitUntil(
+    caches.open(cacheName).then((cache) => {
+      console.log('caching shell assets');
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+self.addEventListener('activate', evt => {
+  evt.waitUntil(
+      caches.keys().then(keys => {
+          return Promise.all(keys
+              .filter(key => key !== cacheName)
+              .map(key => caches.delete(key))
+          );
+      })
+  );
+});
