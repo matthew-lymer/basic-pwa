4f25a045c8f9d46b04561b7b9a294405dd233ba4
matthew-lymer <31863428+matthew-lymer@users.noreply.github.com>
2024-05-01 17:03:01 +0100

    fix

--- js/pwa.js
+++ js/pwa.js
@@ -1,4 +1,4 @@
-var cacheName = 'version-1.09';
+var cacheName = 'version-1.11';
 let newServiceWorker;
 let updateBar = document.getElementById('update');
 let otherUpdateBar = document.getElementById('otherUpdate');
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var cacheName = 'version-1.09';
+var cacheName = 'version-1.11';
 var filesToCache = [
     //HTML
     'index.html?v='+cacheName,
