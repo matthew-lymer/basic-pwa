6822aa12910d1c7e4f8013b13554947d74a80667
matthew-lymer <31863428+matthew-lymer@users.noreply.github.com>
2024-05-01 16:00:14 +0100

    fix db import

--- js/pwa.js
+++ js/pwa.js
@@ -1,4 +1,4 @@
-var cacheName = 'version-1.07';
+var cacheName = 'version-1.08';
 let newServiceWorker;
 let updateBar = document.getElementById('update');
 let otherUpdateBar = document.getElementById('otherUpdate');
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var cacheName = 'version-1.07';
+var cacheName = 'version-1.08';
 var filesToCache = [
     //HTML
     'index.html?v='+cacheName,
